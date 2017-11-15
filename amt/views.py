144b782757026c22d7b1a92718ec019984dd64b4
xzdylyh <lengyaohui@163.com>
2017-11-15 08:59:17 +0800

    run dic add __init__

--- amt/views.py
+++ amt/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from models import case_interface_table,run_interface_table
 from django.shortcuts import render,render_to_response
 from django.http import request
 # Create your views here.
@@ -8,4 +8,18 @@ def case_manage_page(request):
     return render(request,"casemanage.html")
 
 def case_manage_iface(request):
-    return render(request,"iface.html")
+    posts = case_interface_table.objects.all()
+    if request.method=='POST':
+        desc = request.POST.get('idesc')
+        url = request.POST.get('iurl')
+        '''
+        增
+        models.UserInfo.objects.create(user='yangmv',pwd='123456')
+        或者
+        obj = models.UserInfo(user='yangmv',pwd='123456')
+        obj.save()
+        或者
+        dic = {'user':'yangmv','pwd':'123456'}
+        models.UserInfo.objects.create(**dic)
+        '''
+    return render(request,"iface.html",{"posts":posts,})
