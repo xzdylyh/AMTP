d8212b52473af6de3aafa4c37f2227f16b4d5f72
huihui <lengyaohui@163.com>
2018-03-14 17:23:30 +0800

    20180228 modfily

--- amt/views.py
+++ amt/views.py
@@ -109,9 +109,9 @@ def login_validation(request):
         if uname==user.username and password==user.password:
             request.session['username']=uname
             request.session.set_expiry(600) #session过期时间
-            return render(request,'index.html')
+        return JsonResponse({'res':1}) #1验证成功
     except Exception as ex:
-        return JsonResponse({'res':'用记名或密码错误'})
+        return JsonResponse({'res':0}) #0验证失败
 
 #用户注册
 def register(request):
