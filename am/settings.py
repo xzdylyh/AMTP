c89f24ffe35e68a7cd36b615649c34f81212f7e5
huihui <lengyaohui@163.com>
2018-03-16 15:30:33 +0800

    20180228 modfily

--- am/settings.py
+++ am/settings.py
@@ -121,11 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_DIR=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS=(
-    ("css",os.path.join(STATIC_DIR,'css')),
-    ("js",os.path.join(STATIC_DIR,'js')),
-    ("images",os.path.join(STATIC_DIR,'images')),
-)
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=(
+    ("css",os.path.join(STATIC_ROOT,'css')),
+    ("js",os.path.join(STATIC_ROOT,'js')),
+    ("images",os.path.join(STATIC_ROOT,'images')),
+    ("font",os.path.join(STATIC_ROOT,'font')),
+)
+
 LOGIN_URL = '/login/'
